516804e1578face68c73555144b93a0ae86031d3
Richard Maxwell <richard@rsmaxwell.co.uk>
2018-07-19 10:25:42 +0100

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 x=target/classes
-x=${x};${HOME}.m2/repository/org/json/json/20180130/json-20180130.jar
+x=${x}:/cygdrive/c/Users/Richard/.m2/repository/org/json/json/20180130/json-20180130.jar
 CLASSPATH=${x}
 
+set -x
 java -classpath ${CLASSPATH} com.rsmaxwell.pyrunner.Main
